922b24c929f91aff5379124490326c2761770b51
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-25 21:23:43 -0500

    generated rooms model, controller, view

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_24_225859) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_26_014608) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
