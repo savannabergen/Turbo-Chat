240a05525a64caf6e17a28e3396669faa68e48e3
Savanna Bergen <savanna.bergen@gmail.com>
2024-10-25 22:54:48 -0500

    trying to fix the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_10_26_014608) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.string "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.boolean "is_private", default: false
